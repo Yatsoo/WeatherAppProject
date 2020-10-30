7044f30c3e21f5a4866c1c0d18512beb389ff588
Yatsoo <Yatsoo@users.noreply.github.com>
2020-10-30 22:40:34 +0100

    displaying current temperature

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-let apiKey = "75d95a41dc10b1a429fa3263e1c83647";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°C`;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#windSpeed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
-console.log(apiUrl);
+let apiKey = "75d95a41dc10b1a429fa3263e1c83647";
+let city = "Dublin";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
